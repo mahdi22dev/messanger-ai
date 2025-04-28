fb65b0bee326eef6d76aa0ed878b073cf4b7b8a2
mahdi22dev <idrissimahdi2020@gmail.com>
2025-04-28 17:44:38 +0100

    adding web search function for Gemini

--- controllers/postWebHook.js
+++ controllers/postWebHook.js
@@ -1,6 +1,5 @@
 const { default: axios } = require("axios");
 const { GoogleGenAI } = require("@google/genai");
-const { HfInference } = require("@huggingface/inference");
 const { Together } = require("together-ai");
 const {
   insertUserContext,
@@ -177,7 +176,11 @@ const createAIModel = (type, apiKey, hf, tg, senderId, image) => {
             {
               text: `You are a helpful and friendly chatbot assistant designed for Facebook Messenger.\\n\\nMessenger does not support Markdown or HTML formatting. Therefore:\\n\\nDo not use bold **text**, italics _text_, headings #, or code blocks \`code\`.\\n\\nAll responses must be in plain text only.\\n\\nFor lists, use simple characters like:\\n\\n*, -, or numbers (1., 2.) for item markers.\\n\\nBe concise, conversational, and easy to understand.\\n\\nIf presenting steps or options, make them clearly visible using line breaks and list markers.\\n\\nEmojis are okay ✅ and can be used to add tone or clarity,\\n\\nHere’s what you can do:\\n* Check your account status\\n* View recent activity\\n* Contact support\\n\\nAlways adapt your tone to be helpful, polite, and human-friendly — just like you're chatting with a friend via Messenger.`,
             },
+            {
+              text: `When users ask about URLs, provide detailed descriptions of the content instead of links.`,
+            },
           ],
+          tools: [{ googleSearch: {} }],
         };
         const model = "gemini-2.0-flash";
         const history = await getConversationHistory(senderId);
